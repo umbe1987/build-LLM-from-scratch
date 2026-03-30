fd17bb71e995885e76020460750f329ebd2fbc1f
umbe1987 <umbertofilippo@tiscali.it>
2026-03-30 15:36:47 +0200

    self-attention mechanism

--- chap_3.py
+++ chap_3.py
@@ -67,3 +67,94 @@ print(all_context_vecs)
 print("Previous 2nd cotext vector:", context_vec_2)
 
 ## 3.4 Implementing self-attention with trainable weights
+x_2 = inputs[1] # second input element
+d_in = inputs.shape[1] # input embedding size (d=3)
+d_out = 2 # output embedding size (d_out=2)
+
+# initialize the three weighting matrices
+torch.manual_seed(123)
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key   = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+# compute query, key and value vectors
+query_2 = x_2 @ W_query
+key_2   = x_2 @ W_key
+value_2 = x_2 @ W_value
+print(query_2)
+
+# key and value vectors for all input elements are required for computing the attention weights with respect to the query q_2
+keys = inputs @ W_key
+values = inputs @ W_value
+print("keys.shape:", keys.shape)
+print("values.shape:", values.shape)
+
+# we can calculate the attention score with respect to query 2 is calculcated by dot product of q_2 with the keys
+keys_2 = keys[1]
+attn_scores_22 = query_2.dot(keys_2)
+print(attn_scores_22) # (unnormalized) attention score only for element 2
+
+# all attention scores given query 2
+attn_scores_2 = query_2 @ keys.T
+print(attn_scores_2)
+
+# from (unscaled) attention scores to (scaled) attention weights using softmax
+d_k = keys.shape[-1]
+attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1) # the scale by embedding dimension of the keys is to prevent small gradients
+print(attn_weights_2)
+
+# final step: calculathe the context vectors
+context_vec_2 = attn_weights_2 @ values # the attention weights serve as a weighting factor that weighs the repsective importance of each value vector
+print(context_vec_2)
+
+# compact self-attention class
+import torch.nn as nn
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    # forward pass function
+    def forward(self, x):
+        keys    = x @ self.W_key
+        queries = x @ self.W_query
+        values  = x @ self.W_value
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+    
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+
+# improved self-attention class using PyTorch's Linear layers (which provides a more sophisticated weight initialization scheme)
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    # forward pass function
+    def forward(self, x):
+        keys    = self.W_key(x)
+        queries = self.W_query(x)
+        values  = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+    
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
+
+## 3.5 Hiding future words with causal attention
