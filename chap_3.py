af2719e8964368f44de75a78a24c313f343b9239
umbe1987 <umbertofilippo@tiscali.it>
2026-04-16 14:51:50 +0200

    chapter-3: multi-head attention (ex3.3 does not work)

--- chap_3.py
+++ chap_3.py
@@ -247,3 +247,112 @@ context_vecs = ca(batch)
 print("context_vecs.shape:", context_vecs.shape)
 
 ## 3.6 Extending single-head attention to multi-head attention
+## Stacking multiple single-head attention layers
+# a simple way to have multipl single-head attention in parallel is to stack multiple CausalAttention modules in a wrapper module
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length,
+            dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(
+                d_in, d_out, context_length, dropout, qkv_bias
+            )
+            for _ in range(num_heads)]
+        )
+
+    def forward(self, x):
+        # this processes the heads sequentially
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+
+torch.manual_seed(123)
+context_length = batch.shape[1] # This is the number of tokens
+d_in, d_out = 3, 2
+mha = MultiHeadAttentionWrapper(
+    d_in, d_out, context_length, 0.0, num_heads = 2
+)
+context_vecs = mha(batch)
+
+print(context_vecs)
+print("context_vecs.shape:", context_vecs.shape)
+
+## Implementing multi-head attention with weight splits
+# this (re)implementation of multihead attention class takes advantage of
+# transpose and reshape of tensors to allow matrix multiplication.
+# the calculations are not sequential anymore, and we only need only one matrix multiplication, which is more efficient! 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out,
+                context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), \
+            "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads # mattch the desired output dimension
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out) # Linear layer combining the head outputs
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length),
+            diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        # split the matrix by adding num_heads dimension and unroll last dimension
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        # transpose shape
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+        # compute dot product (@) for each head
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens] # this truncates the mask to the number of tokens
+
+        attn_scores.masked_fill(mask_bool, -torch.inf) # fill the attention score using the mask
+
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1, 2)
+        # context vectors from all heads are transposed back to the original (flattened) shape
+        context_vec = context_vec.contiguous().view(
+            b, num_tokens, self.d_out
+        )
+        # add linear projection (optional, commonly used in LLM architectures)
+        context_vec = self.out_proj(context_vec)
+
+        return context_vec
+
+torch.manual_seed(123)
+batch_size, context_length, d_in = batch.shape
+d_out = 2
+mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+context_vecs = mha(batch)
+print(context_vecs)
+print("context_vecs.shape:", context_vecs.shape)
+
+## Exercise 3.3 Initializing GPT-2 size attention modules
+# Using the MultiHeadAttention class, initialize a multi-head attention module that
+# has the same number of attention heads as the smallest GPT-2 model (12 attention
+# heads). Also ensure that you use the respective input and output embedding sizes
+# similar to GPT-2 (768 dimensions). Note that the smallest GPT-2 model supports a
+# context length of 1,024 tokens.
+torch.manual_seed(123)
+batch_size = batch.shape[0]
+context_length = 1024
+d_in = d_out = 768
+mha_ex3_3 = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=12)
+context_vecs = mha_ex3_3(batch)
+print(context_vecs)
+print("context_vecs.shape:", context_vecs.shape)
