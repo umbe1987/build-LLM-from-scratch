36a622ce1e210f747fda16f5fab0580685b8e215
umbe1987 <umbertofilippo@tiscali.it>
2026-04-30 16:22:30 +0200

    chapter-4: layer normalization, GELU activation, and feed forward network

--- chap_4.py
+++ chap_4.py
@@ -81,3 +81,96 @@ print("Output shape:", logits.shape)
 print(logits)
 
 ## 4.2 Normalizing activations with layer normalization
+torch.manual_seed(123)
+batch_example = torch.randn(2, 5)
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Normalized layer outputs:\n", out_norm)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# turn off scientific notation
+torch.set_printoptions(sci_mode=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        
+        return self.scale * norm_x + self.shift
+
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, keepdim=True, unbiased=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+## 4.3 Implementing a feed forward network with GELU activations
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+    
+# compare GELU and ReLU side by side with plots
+import matplotlib.pyplot as plt
+gelu, relu = GELU(), nn.ReLU()
+
+x = torch.linspace(-3, 3, 100)
+y_gelu, y_relu = gelu(x), relu(x)
+plt.figure(figsize=(8, 3))
+for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+    plt.subplot(1, 2, i)
+    plt.plot(x, y)
+    plt.title(f"{label} activation funciton")
+    plt.xlabel("x")
+    plt.ylabel(f"{label} (x)")
+    plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# implement a feed forward neural network module
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            # inputs are projected into a four-times larger space to explore a richer representation space
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            # the output layer shrinks back to the original dimension to match the input
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
+    
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768) # sample input with batch dimension 2
+out = ffn(x)
+print(out.shape)
+
+## 4.4 Adding shortcut connections
