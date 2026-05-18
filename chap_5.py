17d7405cd862099fd51cac27f4c341aeb15a3b31
umbe1987 <umbertofilippo@tiscali.it>
2026-05-18 15:48:53 +0200

    chapter-5: temperature scaling and top-k sampling to control model 'creativity'

--- chap_5.py
+++ chap_5.py
@@ -288,3 +288,142 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
 
 epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
 plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+## 5.3 Decoding strategies to control randomness
+model.to("cpu")
+model.eval() # turns off random components such as dropout
+
+# plug the model instance into the generate_text_simple function to generate one token at a time
+tokenizer = tiktoken.get_encoding("gpt2")
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=25,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+## 5.3.1 Temperature scaling
+# small vocabulary to illustrate next-token generation probabilistic sampling
+vocab = {
+    "closer": 0,
+    "every": 1,
+    "effort": 2,
+    "forward": 3,
+    "inches": 4,
+    "moves": 5,
+    "pizza": 6,
+    "toward": 7,
+    "you": 8
+}
+inverse_vocab = {v: k for k, v in vocab.items()}
+# assume the LLM is given the context "every effort moves you" and generates the following logits
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+probas = torch.softmax(next_token_logits, dim=0)
+next_token_id = torch.argmax(probas).item()
+print(inverse_vocab[next_token_id])
+
+# replace argmax with multinomial function to implement a probabilistic sampling process
+torch.manual_seed(123)
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+print(inverse_vocab[next_token_id])
+
+# multinomial will select the token with the highest probability score most of the times, but NOT ALWAYS as can be seen in the loop
+def print_sampled_tokens(probas):
+    torch.manual_seed(123)
+    sample = [torch.multinomial(probas, num_samples=1).item()
+              for i in range(1_000)]
+    sampled_ids = torch.bincount(torch.tensor(sample))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")
+
+print_sampled_tokens(probas)
+
+# temperature scaling, a.k.a dividing the logits by a number greater than 0 to further control the distribution process
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    
+    return torch.softmax(scaled_logits, dim=0)
+
+temperatures = [1, 0.1, 5] # original, lower and higher confidence
+scaled_probas = [softmax_with_temperature(next_token_logits, T) 
+                 for T in temperatures]
+x = torch.arange(len(vocab))
+bar_width = 0.15
+fig, ax = plt.subplots(figsize=(5, 3))
+for i, T in enumerate(temperatures):
+    rects = ax.bar(x + i * bar_width, scaled_probas[i],
+                   bar_width, label=f'Temperature = {T}')
+ax.set_ylabel('Probability')
+ax.set_xticks(x)
+ax.set_xticklabels(vocab.keys(), rotation=90)
+ax.legend()
+plt.tight_layout()
+plt.show()
+
+## Exercise 5.1
+for i, T in enumerate(temperatures):
+    print(f"Probabilities with T = {T}: {print_sampled_tokens(scaled_probas[i])}\n")
+
+## 5.3.2 Top-k sampling
+# top-k approach allows masking low ranked tokens so that when combined with temeperature trhe result 
+# can be more 'creative' but at the same time reducing the possibility to generate nonsensical phrases
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+print("Top logits:", top_logits)
+print("Top positions:", top_pos)
+
+# set the logit values of tokens lower than the top-k selected to negaitve infinity
+new_logits = torch.where(
+    condition=next_token_logits < top_logits[-1],
+    input=torch.tensor(float('-inf')),
+    other=next_token_logits
+)
+print(new_logits)
+
+# apply softmax to the masked logits to turn the scores into next-token probabilities
+topk_probas = torch.softmax(new_logits, dim=0)
+print(topk_probas)
+
+## 5.3.3 Modifying the text generation function
+# combine temperature scaling and top-k sampling to create a new generate text function
+def generate(model, idx,max_new_tokens, context_size,
+             temperature=0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val,
+                torch.tensor(float('-inf')).to(logits.device),
+                logits
+            )
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == eos_id: # stops generate early if end-of-sequence token is encountered
+            break
+        idx = torch.cat((idx, idx_next), dim=1)
+    
+    return idx
+
+torch.manual_seed(123)
+token_ids = generate(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer),
+    max_new_tokens=15,
+    context_size=GPT_CONFIG_124M["context_length"],
+    top_k=25,
+    temperature=1.4
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
