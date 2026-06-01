cc4c8e7d3bd7361e7c58f1117bb36adf650860bc
umbe1987 <umbertofilippo@tiscali.it>
2026-06-01 17:01:19 +0200

    chapter-6: initialize and test a pretrained model

--- chap_6.py
+++ chap_6.py
@@ -192,3 +192,61 @@ print(f"{len(val_loader)} validation batches")
 print(f"{len(test_loader)} test batches")
 
 ## 6.4 Initializing a model with pretrained weights
+# model configs (same as previous chapter)
+CHOOSE_MODEL = "gpt2-small (124M)"
+INPUT_PROMPT = "Every effort moves"
+BASE_CONFIG = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "drop_rate": 0.0,
+    "qkv_bias": True
+}
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25}
+}
+BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+# import functions to load a pretrained GPT model as done in previous chapter
+from gpt_download import download_and_load_gpt2
+from chap_5 import GPTModel, load_weights_into_gpt
+
+model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+settings, params = download_and_load_gpt2(
+    model_size=model_size, models_dir="gpt2"
+)
+model = GPTModel(BASE_CONFIG)
+load_weights_into_gpt(model, params)
+model.eval()
+
+# reuse text generation utility function form chapters 4 and 5 to ensure the model generates coherent text
+from chap_4 import generate_text_simple
+from chap_5 import text_to_token_ids, token_ids_to_text
+
+text_1 = "Every effort moves you"
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(text_1, tokenizer),
+    max_new_tokens=15,
+    context_size=BASE_CONFIG["context_length"]
+)
+print(token_ids_to_text(token_ids, tokenizer))
+
+# check that the model can already classify spam messages before fine-tuning
+text_2 = (
+    "Is the following text 'spam'? Answer with 'yes' or 'no'"
+    " 'You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award.'"
+)
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(text_2, tokenizer),
+    max_new_tokens=23,
+    context_size=BASE_CONFIG["context_length"]
+)
+# it is expected that the model struggles to follw instructions since it lacks instruction fine-tuning
+print(token_ids_to_text(token_ids, tokenizer))
+
+## 6.5 Adding a classification head
