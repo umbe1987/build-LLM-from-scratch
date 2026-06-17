03812af4b0f87e406a61a8d52d1367acf0bb74b9
umbe1987 <umbertofilippo@tiscali.it>
2026-06-17 15:53:59 +0200

    chapter-6: fine-tuning the model and use it as spam classifier

--- chap_6.py
+++ chap_6.py
@@ -399,3 +399,194 @@ print(f"Validation loss: {val_loss:.3f}")
 print(f"Test loss: {test_loss:.3f}")
 
 ## 6.7 Fine-tuning the model on supervised data
+# Compared to pretraining, the trianing function for fine-tuning a spam classifier
+# will track the number of training exmples seen instead of tokens, and calculate 
+# the accuracy after each epoch instead of printing a sample text
+def train_classifier_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs, eval_freq, eval_iter):
+    # initilize lists to track losses and example seen
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+    examples_seen, global_step = 0, -1
+
+    # main training loop
+    for epoch in range(num_epochs):
+        # sets model to training mode
+        model.train()
+
+        for input_batch, target_batch in train_loader:
+            # reset loss gradient from previous batch iteration
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            loss.backward() # calculate loss gradient
+            optimizer.step() # updates model weights using loss gradients
+            examples_seen += input_batch.shape[0] # tracks examples (instead of tokens)
+            global_step += 1
+
+            # optional evaluation step
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, "
+                      f"Val loss {val_loss:.3f}")
+        
+        # calculate accuracy after each epoch
+        train_accuracy = calc_accuracy_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_accuracy = calc_accuracy_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+
+        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
+
+# the evaluate_model function is identical to the one for pretraining
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+# initialize the optimizer, set number of epochs, initiate training using the new function
+import time
+
+start_time = time.time()
+torch.manual_seed(123)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5, weight_decay=0.1)
+num_epochs = 5
+
+train_losses, val_losses, train_accs, val_accs, examples_seen = \
+    train_classifier_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs=num_epochs, eval_freq=50, eval_iter=5
+    )
+
+end_time = time.time()
+execution_time_minutes = (end_time - start_time) / 60
+print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+# plot the classification loss
+import matplotlib.pyplot as plt
+
+def plot_values(
+        epochs_seen, examples_seen, train_values, val_values,
+        label="loss"):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    # plot training and validation loss against epochs
+    ax1.plot(epochs_seen, train_values, label=f"Training {label}")
+    ax1.plot(
+        epochs_seen, val_values, linestyle="-.",
+        label=f"Validation {label}"
+    )
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend()
+
+    # creates second axis for examples_seen
+    ax2 = ax1.twiny()
+    # invisible plot for aligning ticks
+    ax2.plot(examples_seen, train_values, alpha=0)
+    ax2.set_xlabel("Examples seen")
+
+    # adjust layout to make room
+    fig.tight_layout()
+    plt.savefig(f"{label}-plot.pdf")
+    plt.show()
+
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+examples_seen_tensor = torch.linspace(0, examples_seen, len(train_losses))
+
+plot_values(epochs_tensor, examples_seen_tensor, train_losses, val_losses)
+
+# plot the classification accuracies
+epochs_tensor = torch.linspace(0, num_epochs, len(train_accs))
+examples_seen_tensor = torch.linspace(0, examples_seen, len(train_accs))
+
+plot_values(
+    epochs_tensor, examples_seen_tensor, train_accs, val_accs,
+    label="accuracy"
+)
+
+# calculate performance metrics across the entire datasets
+train_accuracy = calc_accuracy_loader(train_loader, model, device)
+val_accuracy = calc_accuracy_loader(val_loader, model, device)
+test_accuracy = calc_accuracy_loader(test_loader, model, device)
+
+print(f"Training accuracy: {train_accuracy*100:.2f}%")
+print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+print(f"Test accuracy: {test_accuracy*100:.2f}%")
+
+## 6.8 Using the LLM as a spam classifier
+def classify_review(
+        text ,model, tokenizer, device, max_length=None,
+        pad_token_id=50256):
+    model.eval()
+
+    # prepares inputs to the model
+    input_ids = tokenizer.encode(text)
+    supported_context_length = model.pos_emb.weight.shape[0]
+
+    # truncates sequences if they are too long
+    input_ids = input_ids[:min(
+        max_length, supported_context_length
+    )]
+
+    # pads sequences to the longest sequence
+    input_ids += [pad_token_id] * (max_length - len(input_ids))
+
+    input_tensor = torch.tensor(
+        input_ids, device=device
+    ).unsqueeze(0) # adds batch dimension
+
+    # model inference without gradient tracking
+    with torch.no_grad():
+        # logits of the last output token
+        logits = model(input_tensor)[:, -1, :]
+    predicted_label = torch.argmax(logits, dim=-1).item()
+
+    # return the classified result
+    return "spam" if predicted_label == 1 else "not spam"
+
+# let's try on an example text
+text_1 = (
+    "You are a winner you have been specially"
+    " selected to receive $1000 cash or a $2000 award."
+)
+
+print(classify_review(
+    text_1, model, tokenizer, device, max_length=train_dataset.max_length
+))
+
+text_2 = (
+    "Hey, just wanted to check if we're still on"
+    " for dinner tonight? Let me know!"
+)
+
+print(classify_review(
+    text_2, model, tokenizer, device, max_length=train_dataset.max_length
+))
+
+# save the model in case we need to reuse later without having to train again
+torch.save(model.state_dict(), "review_classifier.pth")
+
+# to load it back one can do
+# model_state_dict = torch.load("review_classifier.pth, map_location=device")
+# model.load_state_dict(model_state_dict)
